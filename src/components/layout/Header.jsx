4b41d46f5b53f8b24655bcc37c2da8691bef1a72
an9383 <an9383@gmail.com>
2024-04-15 08:59:37 +0900

    all

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,11 +7,7 @@ function Header() {
     const location = useLocation()
 
     return (
-<<<<<<< HEAD
-        <header className="flex justify-between items-center border-2 border-gray-600 bg-primary p-5">
-=======
         <header className="flex flex-col items-center justify-between p-5 border-2 border-gray-600 md:flex-row bg-primary">
->>>>>>> 9a802b463370013f9c48469f5e97dad11bf8bd9a
             <h1 className="text-3xl font-bold text-secondary">logo</h1>
             <Gnb>
                 <ul>
@@ -35,25 +31,14 @@ function Header() {
     )
 }
 
-<<<<<<< HEAD
-// const Gnb = tw.nav`
-//     bg-secondary p-3
-// `
-=======
 const Util = styled.div`
     ${tw`hidden md:block`}
 `
->>>>>>> 9a802b463370013f9c48469f5e97dad11bf8bd9a
 
 const Gnb = styled.nav`
     ul {
         ${tw`flex gap-6`}
         border: 1px solid #fff;
-    }
-    li {
-        a {
-            ${tw`text-white`}
-        }
     }
     li {
         a {
